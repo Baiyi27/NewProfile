0dae5198d1bf01a7eb955489778c5dfaab96eae8
baiyi <65945846+Baiyi27@users.noreply.github.com>
2024-02-05 22:11:59 +0800

    adjust
    adjust meslo font

---
+++ gitClean1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Prompt the user to input parameters for the tail command
+read -p "Please enter the parameters for the tail command: " tail_params
+
+# Execute the git verify-pack command, pipe the output to sort and tail commands
+result=$(git verify-pack -v .git/objects/pack/pack-*.idx | sort -k 3 -g | tail -n "$tail_params")
+# echo "$result"
+
+# # Extract the blob information and store it in a variable
+# blob_info=$(echo "$result" | grep "blob")
+
+# # Print the blob information
+# echo "$blob_info"
+
+# Extract the SHA-1 hash from the blob information
+sha1_hash=$(echo "$result" | awk '{print $1}')
+
+# echo "$sha1_hash"
+
+# Execute the git rev-list command to find the object based on the SHA-1 hash
+
+result=$(git rev-list --objects --all | grep "$sha1_hash")
+
+echo "$result"
+
+# file_path=$(git rev-list --objects --all | grep "$sha1_hash" | awk '{print $2}')
+
+# echo "$file_path"
+
+# git filter-branch --index-filter 'git rm --cached --ignore-unmatch ${file_path}'
+
+
